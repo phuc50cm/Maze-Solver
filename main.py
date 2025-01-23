9a6c43b092297f408905f3140b399cb768cda719
phuc50centimeters <186391026+phuc50cm@users.noreply.github.com.>
2025-01-23 17:46:30 +0700

    Build fundamental graphic

--- main.py
+++ main.py
@@ -1,31 +1,39 @@
-from tkinter import Tk, BOTH, Canvas
-
-class Window:
-    def __init__(self, width, height):
-        self.width = width
-        self.height = height
-        self.__root = Tk()
-        self.__root.title = "Maze Solver"
-        self.__root.protocol("WM_DELETE_WINDOW", self.close)
-        canvas = Canvas()
-        canvas.pack()
-        self.running = False
-
-    def redraw(self):
-        self.__root.update_idletasks()
-        self.__root.update()
-
-    def wait_for_close(self):
-        self.running = True
-        while self.running:
-            self.redraw()
-
-    def close(self):
-        self.running = False
+from window import Window
+from line import Line
+from point import Point
+from cell import Cell
 
 def main():
+    # Testing to make sure it work
     win = Window(800, 600)
+
+    # Test drawcell
+    c1 = Cell(win)
+    c1.has_right_wall = False
+    c1.draw(50, 50, 100, 100)
+
+    c2 = Cell(win)
+    c2.has_left_wall = False
+    c2.has_bottom_wall = False
+    c2.draw(100, 50, 150, 100)
+
+    c1.draw_move(c2)
+
+    c3 = Cell(win)
+    c3.has_top_wall = False
+    c3.has_right_wall = False
+    c3.draw(100, 100, 150, 150)
+
+    c2.draw_move(c3)
+
+    c4 = Cell(win)
+    c4.has_left_wall = False
+    c4.draw(150, 100, 200, 150)
+
+    c3.draw_move(c4, True)
+
     win.wait_for_close()
+
 
 if __name__ == "__main__":
     main()
---
+++ window.py
@@ -0,0 +1,30 @@
+from tkinter import Tk, BOTH, Canvas
+
+
+class Window:
+    def __init__(self, width, height):
+        self.width = width
+        self.height = height
+        self.__root = Tk()
+        self.__root.title("Maze Solver")
+        self.__root.protocol("WM_DELETE_WINDOW", self.close)
+        self.__canvas = Canvas(
+            self.__root, bg="white", height=height, width=width
+        )
+        self.__canvas.pack()
+        self.running = False
+
+    def redraw(self):
+        self.__root.update_idletasks()
+        self.__root.update()
+
+    def wait_for_close(self):
+        self.running = True
+        while self.running:
+            self.redraw()
+
+    def close(self):
+        self.running = False
+
+    def draw_line(self, line, fill_color="black"):
+        line.draw(self.__canvas, fill_color)
