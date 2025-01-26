0ee7667f0d5e039a1f05bfed6b709cb1a2719ec6
phuc50centimeters <186391026+phuc50cm@users.noreply.github.com.>
2025-01-26 12:29:36 +0700

    Finish maze and solver

--- cell.py
+++ cell.py
@@ -12,6 +12,7 @@ class Cell():
         self._y1 = None
         self._y2 = None
         self._win = win
+        self.visited = False
 
     def draw(self, x1, y1, x2, y2):
         if self._win is None:
@@ -23,15 +24,27 @@ class Cell():
         if self.has_left_wall:
             line = Line(Point(x1, y1), Point(x1, y2))
             self._win.draw_line(line)
+        else:
+            line = Line(Point(x1, y1), Point(x1, y2))
+            self._win.draw_line(line, "white")
         if self.has_top_wall:
             line = Line(Point(x1, y1), Point(x2, y1))
             self._win.draw_line(line)
+        else:
+            line = Line(Point(x1, y1), Point(x2, y1))
+            self._win.draw_line(line, "white")
         if self.has_right_wall:
             line = Line(Point(x2, y1), Point(x2, y2))
             self._win.draw_line(line)
+        else:
+            line = Line(Point(x2, y1), Point(x2, y2))
+            self._win.draw_line(line, "white")
         if self.has_bottom_wall:
             line = Line(Point(x1, y2), Point(x2, y2))
             self._win.draw_line(line)
+        else:
+            line = Line(Point(x1, y2), Point(x2, y2))
+            self._win.draw_line(line, "white")
 
     def draw_move(self, to_cell, undo=False):
         half_length = abs(self._x2 - self._x1) // 2
@@ -42,7 +55,7 @@ class Cell():
         x_center2 = half_length2 + to_cell._x1
         y_center2 = half_length2 + to_cell._y1
 
-        color = "red" if undo else "gray"
+        color = "gray" if undo else "red"
 
         line = Line(Point(x_center, y_center), Point(x_center2, y_center2))
         self._win.draw_line(line, color)
--- main.py
+++ main.py
@@ -1,39 +1,31 @@
+import sys
+
 from window import Window
 from line import Line
 from point import Point
 from cell import Cell
+from maze import Maze
 
 def main():
-    # Testing to make sure it work
-    win = Window(800, 600)
+    num_rows = 12
+    num_cols = 16
+    margin = 50
+    screen_x = 800
+    screen_y = 600
+    cell_size_x = (screen_x - 2 * margin) / num_cols
+    cell_size_y = (screen_y - 2 * margin) / num_rows
 
-    # Test drawcell
-    c1 = Cell(win)
-    c1.has_right_wall = False
-    c1.draw(50, 50, 100, 100)
+    sys.setrecursionlimit(10000)
+    win = Window(screen_x, screen_y)
 
-    c2 = Cell(win)
-    c2.has_left_wall = False
-    c2.has_bottom_wall = False
-    c2.draw(100, 50, 150, 100)
-
-    c1.draw_move(c2)
-
-    c3 = Cell(win)
-    c3.has_top_wall = False
-    c3.has_right_wall = False
-    c3.draw(100, 100, 150, 150)
-
-    c2.draw_move(c3)
-
-    c4 = Cell(win)
-    c4.has_left_wall = False
-    c4.draw(150, 100, 200, 150)
-
-    c3.draw_move(c4, True)
-
+    maze = Maze(margin, margin, num_rows, num_cols, cell_size_x, cell_size_y, win, 10)
+    print("maze created")
+    is_solvable = maze.solve()
+    if not is_solvable:
+        print("maze can not be solved!")
+    else:
+        print("maze solved!")
     win.wait_for_close()
-
 
 if __name__ == "__main__":
     main()
